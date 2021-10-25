be24d9d242644010fb9cdc81609f305732ab488c
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-25 20:40:23 +0530

    Added:welcome

---
+++ src/main/java/bridelabz/MoodAnalyzer/MoodAnalyser.java
@@ -0,0 +1,10 @@
+package bridelabz.MoodAnalyzer;
+
+public class MoodAnalyser {
+
+	public static void main(String[] args) {
+		System.out.println("welcome");
+
+	}
+
+}
---
+++ src/test/java/bridelabz/MoodAnalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,10 @@
+package bridelabz.MoodAnalyzer;
+
+public class MoodAnalyzerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
